a5926ab04a54cac3ae9ed7f0cddf22eee01ad4df
RCeasar01 <118212073+RCeasar01@users.noreply.github.com>
2026-06-10 00:29:24 -0400

    fix: update all social links to real accounts

--- js/footer.js
+++ js/footer.js
@@ -6,13 +6,14 @@
   const YEAR = new Date().getFullYear();
 
   const SOCIAL = [
-    { icon: '▶', label: 'YouTube',   href: 'https://www.youtube.com/@bannedproductmedia' },
-    { icon: '◉', label: 'Instagram', href: 'https://www.instagram.com/bannedproduct' },
+    { icon: '▶', label: 'YouTube',   href: 'https://www.youtube.com/@bannedproduct' },
+    { icon: '◉', label: 'Instagram', href: 'https://www.instagram.com/rob_ceasar/' },
     { icon: '𝕏', label: 'X/Twitter', href: 'https://x.com/bannedproduct' },
-    { icon: '◈', label: 'TikTok',    href: 'https://www.tiktok.com/@bannedproduct' },
+    { icon: '◈', label: 'TikTok',    href: 'https://www.tiktok.com/@bannedproduct7' },
+    { icon: '◐', label: 'Twitch',    href: 'https://www.twitch.tv/banned_product' },
     { icon: '⬡', label: 'Whatnot',   href: 'https://www.whatnot.com/user/bannedproduct' },
     { icon: '◎', label: 'LinkedIn',  href: 'https://www.linkedin.com/company/bannedproduct-media' },
-    { icon: '◑', label: 'Facebook',  href: 'https://www.facebook.com/bannedproductmedia' },
+    { icon: '◑', label: 'Facebook',  href: 'https://www.facebook.com/robert.ceasar1/' },
   ];
 
   function resolvePath(href) {
